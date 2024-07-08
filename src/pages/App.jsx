b3546f343f8826ea4100cb2aaf17cc50f0b51769
Patrick Melan <patrick.melan@outlook.com>
2024-07-08 15:29:20 -0400

    installs TailwindCSS

---
+++ src/pages/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="text-4xl">
+      <h1>Welcome to Habiteer.</h1>
+      <p>Your habits, in one place.</p>
+    </div>
+  );
+}
+
+export default App;
