db872a0d143906f11fede24f977a0d7731250b35
Patrick Melan <patrick.melan@outlook.com>
2024-07-08 15:48:41 -0400

    Import DaisyUI and png logo

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -4,6 +4,7 @@ function App() {
     <div className="text-4xl">
       <h1>Welcome to Habiteer.</h1>
       <p>Your habits, in one place.</p>
+      <button class="btn btn-primary rounded-full">Click ME!</button>
     </div>
   );
 }
