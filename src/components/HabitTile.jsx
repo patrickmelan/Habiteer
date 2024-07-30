1110af4c5181e12e18c00a8d5115899a3a5aafeb
Patrick Melan <patrick.melan@outlook.com>
2024-07-30 11:46:54 -0400

    working changes AM

--- src/components/HabitTile.jsx
+++ src/components/HabitTile.jsx
@@ -1,3 +1,4 @@
+//imports
 import ProgressBar from "@ramonak/react-progress-bar";
 import { useEffect, useState } from "react";
 import { supabase } from "../sbclient";
@@ -7,34 +8,39 @@ import 'react-toastify/dist/ReactToastify.css';
 const HabitTile = ({id, name, streak, days_goal, times_per_day, hex}) => {
     const [times_today, setToday] = useState([]);
     let today = new Date().toLocaleDateString('en-CA', {timeZone: "UTC"});
-    const [done, setDone] = useState(false);
+    const [varStreak, setStreak] = useState(streak);
+ 
     //const uid = async () => (await supabase.auth.getUser()).data.user.id;
 
     async function log(e) {
         e.preventDefault();
         
-        try {
-            const { data, error } = await supabase
-            .from("date_log")
-            .insert(
-                {
-                    habit_id: id,
-                    user_id: (await supabase.auth.getUser()).data.user.id,
-                    date: today
+        if (times_today && times_per_day > times_today.length) {
+            try {
+                const { data, error } = await supabase
+                .from("date_log")
+                .insert(
+                    {
+                        habit_id: id,
+                        user_id: (await supabase.auth.getUser()).data.user.id,
+                        date: today
+                    }
+                , { returning: 'minimal' }).select('*')
+    
+                if (error) {
+                    console.log("Error found: ", error);
                 }
-            , { returning: 'minimal' }).select('*')
-
-            if (error) {
-                console.log("Error found: ", error);
-            };
-        } catch (error) {
-            alert(error);
+                
+                checkToday();
+            
+            } catch (error) {
+                alert(error);
+            }
         }
-        
-        todayfunc();
+          
     }
 
-    async function todayfunc() {
+    async function checkToday() {
         try {
             const { data, error } = await supabase
             .from("date_log")
@@ -44,9 +50,8 @@ const HabitTile = ({id, name, streak, days_goal, times_per_day, hex}) => {
 
             setToday(data);
 
-            if (!done && times_today == times_per_day) {
+            if (times_today && times_today.length == times_per_day) {
                 addToStreak();
-                setDone(true)
             }
 
         } catch (error) {
@@ -55,13 +60,23 @@ const HabitTile = ({id, name, streak, days_goal, times_per_day, hex}) => {
     }
 
     async function addToStreak() {
+        setStreak(streak + 1);
+        
         try {
             const { data, error } = await supabase
             .from("habits")
-            .update({'streak': streak+1})
+            .update({'streak': varStreak})
             .eq('user_id', (await supabase.auth.getUser()).data.user.id)
+            .eq('id', id)
 
-            streak += 1;
+            if(data) {
+                console.log(data);
+            }
+
+            if(error) {
+                console.log(error)
+            }
+            
 
         } catch (error) {
             alert(error)
@@ -69,24 +84,20 @@ const HabitTile = ({id, name, streak, days_goal, times_per_day, hex}) => {
     }
 
     useEffect(() => {
-        todayfunc();
+        checkToday();
     }, [])
-
-
-
 
     return (
         <div className="flex flex-col border-zinc-900 border-2 shadow-2xl py-4 px-4 rounded-lg w-full h-full">
             <div className="text-left">
-                <ProgressBar completed={Math.round((((days_goal/2)/days_goal)*100))} className="w-full" baseBgColor="rgb(177, 165, 187)" maxCompleted={100} customLabel="" bgColor={hex} />
+                <ProgressBar completed={streak ? Math.round((((streak)/days_goal)*100)) : 87} className="w-full" baseBgColor="rgb(177, 165, 187)" maxCompleted={100} customLabel="" bgColor={hex} />
             </div>
             <div className="text-center">
-                <h1 className="pt-2 text-xl text-white">{name}</h1>
-                <h1>Goal: {days_goal} days</h1>
-                <h1>{times_per_day}x/Day</h1>
-                <h1>You're on a {streak} day streak!</h1>
-                <button style={{'background-color': 'black'}} className="btn my-2" onClick={log}>Log Today ({times_today ? times_today.length : 0}/{times_per_day})</button>
-                <ToastContainer position="bottom-right" />
+                <h1 className="pt-2 text-xl text-white">{name ? name : "{Habit Name}"}</h1>
+                <h1>Goal: {days_goal ? days_goal : "{Goal}"} days</h1>
+                <h1>{times_per_day ? times_per_day : "{Times per day}"}x/Day</h1>
+                <h1>You're on a {varStreak ? varStreak : "{Streak}"} day streak!</h1>
+                <button style={{'background-color': 'black'}} className="btn my-2" onClick={log}>{times_today && times_today.length == times_per_day ? <h1>All Done!</h1> : <h1>Log Today</h1>}({times_today ? times_today.length : 0}/{times_per_day})</button>
             </div>
         </div>
     )
